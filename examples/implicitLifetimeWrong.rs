f7d87a108e2be4c7c4b9323730f3277c405c12bd
giovannivarr <giovannivarr@gmail.com>
2018-02-10 10:13:06 +0100

    Modificato l'esempio implicitLifetimeWrong.rs per renderlo più significativo.

--- examples/implicitLifetimeWrong.rs
+++ examples/implicitLifetimeWrong.rs
@@ -1,12 +1,12 @@
 fn main (){
-    fn dummy(a: &i32) -> &i32 {
+    fn dummy(a: &i32, b:&i32) -> &i32 {
         a
     }
 
     let r;
     {
         let x = 5;
-        r = dummy(&x);
+        r = dummy(&x, &x);
     }
     println!("{}", r);
 }
